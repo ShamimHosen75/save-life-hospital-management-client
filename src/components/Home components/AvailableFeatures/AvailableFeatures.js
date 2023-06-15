965b195e6fc3b319b2c1c0fdfc64c9b77c28658d
Shamim Hosen <85413631+ShamimHosen75@users.noreply.github.com>
2023-06-16 01:38:51 +0600

    Update AvailableFeatures.js
    Images Updated

--- src/components/Home components/AvailableFeatures/AvailableFeatures.js
+++ src/components/Home components/AvailableFeatures/AvailableFeatures.js
@@ -82,37 +82,37 @@ const AvailableFeatures = () => {
       >
         <SwiperSlide>
           <div>
-            <img src='https://doccure-react.dreamguystech.com/template/5f512084522e55e7d0b0558624d98a9a.jpg' className='w-56 a-image'></img>
+            <img src='https://i.ibb.co/5jjR7Mz/Test-feature-02.jpg' className='w-56 a-image'></img>
             <h1 className='text-primary font-bold text-center text-xl'>Test room</h1>
           </div>
         </SwiperSlide>
         <SwiperSlide>
         <div>
-            <img src='https://doccure-react.dreamguystech.com/template/6962bf596e4125886d79ad2872e3d47e.jpg' className='w-40 a-image'></img>
+            <img src='https://i.ibb.co/ftrDv5Q/feature-06.jpg' className='w-40 a-image'></img>
             <h1 className='text-primary font-bold ml-12 text-xl'>Medical</h1>
           </div>
         </SwiperSlide>
         <SwiperSlide>
         <div>
-            <img src='https://doccure-react.dreamguystech.com/template/1eb95d47909144cd3761c65e9c4c776c.jpg' className='w-56 a-image'></img>
+            <img src='https://i.ibb.co/N1YmGWL/feature-01.jpg' className='w-56 a-image'></img>
             <h1 className='text-primary font-bold ml-8 text-xl'>Patient Ward</h1>
           </div>
         </SwiperSlide>
         <SwiperSlide>
         <div>
-            <img src='https://doccure-react.dreamguystech.com/template/83604bf3f6914c5df51fe23d9df9098f.jpg' className='w-36 a-image'></img>
+            <img src='https://i.ibb.co/PwsBg2Y/feature-03.jpg' className='w-36 a-image'></img>
             <h1 className='text-primary font-bold ml-8 text-xl'>ICU</h1>
           </div>
         </SwiperSlide>
         <SwiperSlide>
         <div>
-            <img src='https://doccure-react.dreamguystech.com/template/1f1ad77261b62c3930f3f5e97cb75efd.jpg' className='w-36 a-image'></img>
+            <img src='https://i.ibb.co/sjfP4nX/lab-feature-04.jpg' className='w-36 a-image'></img>
             <h1 className='text-primary font-bold ml-8 text-xl'>Laboratory</h1>
           </div>
         </SwiperSlide>
         <SwiperSlide>
         <div>
-            <img src='https://doccure-react.dreamguystech.com/template/bf33350e232bcb1c47c9b10ab9626ca6.jpg' className='w-36 a-image'></img>
+            <img src='https://i.ibb.co/9yXQsRT/operation-feature-05.jpg' className='w-36 a-image'></img>
             <h1 className='text-primary font-bold ml-8 text-xl'>Operation</h1>
           </div>
         </SwiperSlide>
